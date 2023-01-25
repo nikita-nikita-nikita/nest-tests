4736ac680ee67d50ca2eb3460a7098573ab2a120
Никита Блудов <neymarenko123@gmail.com>
2023-01-25 19:53:25 +0000

    finish

--- src/queue/queue.service.ts
+++ src/queue/queue.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
+export const timer = (delay, toReturn = null) => new Promise(resolve => setTimeout(() => {
+          resolve(toReturn);
+      }, delay))
+
 class Queue<T, V> {
   queueSize: number;
   queueTimeout: number;
   maxWorkersSum: number;
   handler: (arg: T) => Promise<V>;
+  tasksToDo = new Set();
   stack = new Set<Promise<unknown>>();
   result: V[] = [];
   tasksInProcess = 0;
@@ -19,30 +24,37 @@ class Queue<T, V> {
   clear() {
     this.stack.clear();
   }
+  // there should be to type of adding task or some another mechanizm of adding tasks
   async push(task: T) {
     const addedTaskIndex = this.index;
     this.index += 1;
     if (this.stack.size >= this.queueSize) {
       throw new Error('to many tasks');
     }
-    if (this.tasksInProcess === this.maxWorkersSum) {
+    if (this.tasksInProcess > this.maxWorkersSum) {
       throw new Error('to many going in parallesls');
     }
     const taskToDo = async () => {
-        this.result[addedTaskIndex] = await this.handler(task);
+      if (this.tasksInProcess === this.maxWorkersSum) {
+        await Promise.any(Array.from(this.stack));
+      }
+      this.tasksInProcess++;
+      this.result[addedTaskIndex] = await this.handler(task);
     };
     const promise = taskToDo();
+    this.tasksToDo.add(promise);
     this.stack.add(promise);
-    promise.then(() => this.stack.delete(promise));
+    promise.then(() => {
+      this.stack.delete(promise);
+      this.tasksInProcess--;
+    });
   }
-  
-  timer(){
-      return new Promise(resolve => setTimeout(() => {
-          resolve(this.timeoutErrorSymbol);
-      }, this.queueTimeout))
+  timer() {
+    return timer(this.queueTimeout, this.timeoutErrorSymbol);
   }
   async getResult(){
-      const result = await Promise.race([Promise.all(Array.from(this.stack)), this.timer()]);
+      const result = await Promise.race([Promise.all(Array.from(this.tasksToDo)), this.timer()]);
+      
       if(result === this.timeoutErrorSymbol){
           return "error"
       }
